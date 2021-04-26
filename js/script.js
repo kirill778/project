fbeecb174e76ab02e35e6698336a06a42a69054e
Kirll <example@gmail.com>
2021-04-25 18:19:41 -0700

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
+//Task 1
+
 let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?");
+
+//Task 2
 
 let personalMovieDB = {
 
@@ -12,4 +16,17 @@ let personalMovieDB = {
 
 };
 
-console.log(personalMovieDB.count);
+// Task 3
+
+let lastOfFilms = prompt('Один из последних просмотренных фильмов?');
+let estimation = prompt('На сколько оцените его?');
+
+let movies = {
+
+    lastOfFilms: lastOfFilms,
+    estimation: estimation,
+
+};
+
+
+
